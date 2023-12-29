3ab70434ccee2b80f3a5c169dfa4dde7c7e2a3f5
lijiawei <ljw@live.jp>
2023-12-29 23:01:28 +0800

    feat: 🔰 vue

--- packages/sanya/src/preset.ts
+++ packages/sanya/src/preset.ts
@@ -1,9 +1,10 @@
 export default () => {
   return {
+    presets: [require.resolve("@umijs/preset-vue")],
     plugins: [
-      require.resolve('./plugins/sanyaAlias'),
-      require.resolve('./plugins/sanyaAppData'),
-      require.resolve('./plugins/sanyaChecker'),
+      require.resolve("./plugins/sanyaAlias"),
+      require.resolve("./plugins/sanyaAppData"),
+      require.resolve("./plugins/sanyaChecker"),
     ],
   };
 };
