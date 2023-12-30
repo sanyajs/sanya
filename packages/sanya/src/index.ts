4a7b3f5303a5b1b233fdfd8c5438e86b9b38937a
plus <ljw@live.jp>
2023-12-30 18:01:39 +0800

    feat: 🃏first pr (#1)
    * feat: 🔰 vue
    
    * feat: 🧈 preset
    
    * chore: 🧼 clean
    
    * feat: 🧟 first demo

--- packages/sanya/src/index.ts
+++ packages/sanya/src/index.ts
@@ -1,1 +1,8 @@
-export * from 'umi';
+import { IServicePluginAPI, PluginAPI } from '@umijs/core';
+
+export { run } from './cli/cli';
+export { RUNTIME_TYPE_FILE_NAME } from './constants';
+export { defineConfig } from './defineConfig';
+export { defineMock } from './defineMock';
+export * from './service/service';
+export type IApi = PluginAPI & IServicePluginAPI;
--- packages/sanya/src/preset.ts
+++ packages/sanya/src/preset.ts
@@ -1,9 +1,6 @@
 export default () => {
   return {
-    plugins: [
-      require.resolve('./plugins/sanyaAlias'),
-      require.resolve('./plugins/sanyaAppData'),
-      require.resolve('./plugins/sanyaChecker'),
-    ],
+    presets: [],
+    plugins: [],
   };
 };
